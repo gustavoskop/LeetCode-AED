ca6ef5765cb102f23ca498ede1103fd24119fd4a
gustavoskop <gkopereck@gmail.com>
2025-04-28 20:08:28 -0300

    subarrays

--- easy/subarrays.c
+++ easy/subarrays.c
@@ -1,32 +1,30 @@
 // https://leetcode.com/problems/count-subarrays-of-length-three-with-a-condition/
 
-
 #include <stdio.h>
 
+int countSubarrays(int *nums, int numsSize);
 
-int countSubarrays(int* nums, int numsSize);
-
-int main(){
+int main()
+{
 
     int nums[] = {1, 2, 1, 4, 1}, numsSize = 5;
-    //int nums[] = {1, 1, 1}, numsSize = 3;
+    // int nums[] = {1, 1, 1}, numsSize = 3;
 
     printf("%d", countSubarrays(nums, numsSize));
-
 }
 
-
-
-int countSubarrays(int* nums, int numsSize) {
+int countSubarrays(int *nums, int numsSize)
+{
     int i, n1, n2, n3, count = 0;
 
-    for (i = 0; i < (numsSize - 2); i++ ){
+    for (i = 0; i < (numsSize - 2); i++)
+    {
         n1 = nums[i];
         n2 = nums[i + 1];
         n3 = nums[i + 2];
 
         if ((n1 + n3) * 2 == n2)
-        count++;
+            count++;
     }
     return count;
 }
