e21575473114cd7ab32497f4d3ae2b2710ea6618
gustavoskop <gkopereck@gmail.com>
2025-04-28 20:02:08 -0300

    palindrome_number

--- easy/palindrome-number.c
+++ easy/palindrome-number.c
@@ -1,20 +1,39 @@
+// https://leetcode.com/problems/palindrome-number/
 
+#include <stdio.h>
+#include <string.h>
+#include <stdbool.h>
 
-bool isPalindrome(int x) {
-    char y[12],p[12];
+bool isPalindrome(int x);
+
+int main()
+{
+
+    int x = 121;
+    int x = -121;
+    int x = 10;
+
+    if (isPalindrome(x))
+        printf("true");
+    else
+        printf("false");
+}
+
+bool isPalindrome(int x)
+{
+    char y[12], p[12];
     sprintf(p, "%d", x);
     int i, n, tam;
     tam = strlen(p);
     n = tam - 1;
     for (i = 0; i < tam; i++)
         y[n--] = p[i];
-    
+
     y[i] = '\0';
 
     if (strcmp(y, p) == 0)
-    return true;
-    
+        return true;
+
     else
-    return false;
-    
+        return false;
 }
