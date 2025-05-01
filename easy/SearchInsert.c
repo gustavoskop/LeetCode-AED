d1b3fbb068c5ab6c2c75634ca2141acf11d2c54a
gustavoskop <gkopereck@gmail.com>
2025-05-01 17:53:55 -0300

    searchInsert

--- easy/SearchInsert.c
+++ easy/SearchInsert.c
@@ -6,8 +6,8 @@ int searchInsert(int* nums, int numsSize, int target);
 
 int main(){
     int nums[] = {1, 3, 5, 6}, numsSize = 4;
-    //int target = 5;
-    int target = 2;
+    int target = 5;
+    //int target = 2;
     //int target = 7;
 
     printf("%d", searchInsert(nums, numsSize, target));
