6b72ba980ceb12196c37c1f6d35492f673ee0ddd
gustavoskop <gkopereck@gmail.com>
2025-05-14 14:22:35 -0300

    insertGreatestComoonDivisors

--- medium/insertGreatestCommonDivisors.c
+++ medium/insertGreatestCommonDivisors.c
@@ -5,36 +5,40 @@
  *     struct ListNode *next;
  * };
  */
+
+// https://leetcode.com/problems/insert-greatest-common-divisors-in-linked-list/
+
 struct ListNode* insertGreatestCommonDivisors(struct ListNode* head) {
-    int i, count;
+    int valor = 0, count, menor = 0, maior = 0;
     struct ListNode *p, *q, *new;
 
     p = head;
     q = p->next;
 
- 
-    while(q != NULL){
-        if (p->val % q->val == 0){
-            new = (struct ListNode *) malloc (sizeof(struct ListNode));
+    while (q != NULL) {
+        new = (struct ListNode*)malloc(sizeof(struct ListNode));
 
-            new->val = q->val;
-            
-            new->next = q;
-            p->next = new;
-
-            p = q;
-            q = q->next;
+        if (p->val > q->val) {
+            menor = q->val;
+            maior = p->val;
+        } else {
+            menor = p->val;
+            maior = q->val;
         }
 
+        if (maior % menor == 0)
+            new->val = menor;
+        else
 
+            for (count = 1; count < menor; count++)
+                if ((maior % count) == 0 && (menor % count) == 0)
+                    new->val = count;
 
+        new->next = q;
+        p->next = new;
 
-
-        return head;
-}
-
-
-    
+        p = q;
+        q = q->next;
+    }
     return head;
-
 }
