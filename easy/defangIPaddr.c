869ae7395d9a38f4e49ebde34bdac3ecb9197e70
gustavoskop <gkopereck@gmail.com>
2025-05-16 16:13:33 -0300

    defangIPaddr

---
+++ easy/defangIPaddr.c
@@ -0,0 +1,25 @@
+
+
+char * defangIPaddr(char * address){
+    
+    char *numero = (char*) malloc (sizeof(char) * strlen(address) + 7);
+    int i, k = 0;
+
+    for(i = 0; i < strlen(address); i++){
+        if(address[i] == '.'){
+            numero[k++] = '[';
+            numero[k++] = '.';
+            numero[k++] = ']';
+
+        }
+        else{
+            numero[k] = address[i];
+            k++;
+
+        }
+    }
+
+    numero[k] = '\0'; 
+    return numero;
+
+}
