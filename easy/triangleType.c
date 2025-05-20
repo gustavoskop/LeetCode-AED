dad2dccdbbf50b24c4d5bd0301940bed57a40fb0
gustavoskop <gkopereck@gmail.com>
2025-05-20 19:15:04 -0300

    triangleType

--- easy/triangleType.c
+++ easy/triangleType.c
@@ -1,3 +1,5 @@
+//https://leetcode.com/problems/type-of-triangle/description/
+
 char* triangleType(int* nums, int numsSize) {
     
     if(nums[0] + nums[1] > nums[2] && nums[1] + nums[2] > nums[0] && nums[0] + nums[2] > nums[1]){
