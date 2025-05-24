951d73d8fadf35d87357de261cad8f271b605986
gustavoskop <gkopereck@gmail.com>
2025-05-24 19:02:14 -0300

    feito em casa

--- Trabalhos Práticos M1/trabalhoPraticoM1_final.c
+++ Trabalhos Práticos M1/trabalhoPraticoM1_final.c
@@ -1,9 +1,9 @@
 char *shortestCompletingWord(char *licensePlate, char **words, int wordsSize)
 {
-    int i, k = 0, j, m = 0, n = 0, count = 0, inter = 0, tam = 0, l;
+    int i, k = 0, j, m = 0, inter = 0, tam = 0, l;
     char w[16], tmp[16];
 
-    for (i = 0; i < strlen(licensePlate); i++)
+    for ( i = 0; i < strlen( licensePlate ); i++ )
     {
         if (licensePlate[i] == '1' || licensePlate[i] == '2' ||
             licensePlate[i] == '3' || licensePlate[i] == '4' ||
@@ -13,53 +13,49 @@ char *shortestCompletingWord(char *licensePlate, char **words, int wordsSize)
             licensePlate[i] == ' ')
             continue;
         else
-            w[k++] = tolower(licensePlate[i]);
+            w[k++] = tolower( licensePlate[i] );
     }
     w[k] = '\0';
 
-    for (i = 0; i < wordsSize; i++)
+    for ( i = 0; i < wordsSize; i++ )
     {
         k = 0;
-        n = 0;
         inter = 0;
 
-        for (l = 0; l < strlen(words[i]); l++)
+        for ( l = 0; l < strlen(words[i]); l++ )
             tmp[l] = words[i][l];
 
-        while (w[k] != '\0')
+        while ( w[k] != '\0' )
         {
-            n = k;
-            count = 0;
 
-            for (j = 0; j < strlen(words[i]); j++)
+            for ( j = 0; j < strlen( words[i] ); j++ )
             {
-                if (words[i][j] == w[n])
+                if ( words[i][j] == w[k] )
                 {
                     k++;
                     words[i][j] = '#';
                     break;
                 }
             }
-
             inter++;
-            if (inter == strlen(words[i]))
+            if ( inter == strlen(words[i]) )
                 break;
         }
 
-        if (w[k] == '\0')
+        if ( w[k] == '\0' )
         {
-            if (tam == 0)
+            if ( tam == 0 )
             {
-                tam = strlen(words[i]);
+                tam = strlen( words[i] );
                 m = i;
             }
-            else if (tam > strlen(words[i]))
+            else if ( tam > strlen(words[i]) )
             {
-                tam = strlen(words[i]);
+                tam = strlen( words[i] );
                 m = i;
             }
         }
-        for (l = 0; l < strlen(words[i]); l++)
+        for ( l = 0; l < strlen( words[i] ); l++ )
             words[i][l] = tmp[l];
     }
 
